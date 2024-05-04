a9debba828464bed6144388c3a44967d2424875e
kammila-tilak <kammilatilak@gmail.com>
2024-05-04 18:45:23 +0530

    updated code

--- scripts/10-RPS.js
+++ scripts/10-RPS.js
@@ -95,13 +95,13 @@ function pickComputerMove() {
     let computerMove = '';
 
     if (randomNumber >= 0 && randomNumber < 1 / 3) {
-        computerMove = 'Rock';
+        computerMove = 'rock';
     }
     else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-        computerMove = 'Paper';
+        computerMove = 'paper';
     }
     else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-        computerMove = 'Scissors';
+        computerMove = 'scissors';
     }
 
     return computerMove;
